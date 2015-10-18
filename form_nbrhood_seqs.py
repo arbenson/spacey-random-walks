d24bd90c1f123160f261af3c310393230e180f93
arbenson <arbenson@gmail.com>
2015-10-18 15:43:20 -0400

    update

--- form_nbrhood_seqs.py
+++ form_nbrhood_seqs.py
@@ -1,8 +1,10 @@
-import json
-from shapely.geometry import shape, Point
 from collections import defaultdict
 import csv
+import json
+from shapely.geometry import shape, Point
+import sys
 import time
+
 
 '''
 Read the trip data and convert latlongs to a sequence of neighborhoods
@@ -25,7 +27,7 @@ def GetNeighborhoods():
         nbrhood_polys.append((poly, nbrhood))
     return nbrhood_polys
 
-def FindNeighborhood(lat, long):
+def FindNeighborhood(lat, long, nbrhood_polys):
     ''' Given a latitude and longitude, return the neighborhood. '''
     pt = Point(long, lat)
     for poly, nbrhood in nbrhood_polys:
@@ -33,35 +35,50 @@ def FindNeighborhood(lat, long):
             return nbrhood
     return 'OTHER'
 
-def datetime(dt):
+def format_datetime(dt):
     ''' Return time struct from time string in data files. '''
     return time.strptime(dt, '%Y-%m-%d %H:%M:%S')
 
+def ProcessSeq(seq):
+    ''' Process list of (pickup_time, pickup_nbrhood, dropoff_nbrhood) '''
+    seq.sort()
+    proc_seq = [seq[0][1], seq[0][2]]
+    for i in xrange(1, len(seq)):
+        curr_pickup = seq[i][1]
+        curr_dropoff = seq[i][2]
+        # Put the pickup in the sequence if it changed
+        # from the last point.
+        if curr_pickup != seq[i - 1][2]:
+            proc_seq.append(curr_pickup)
+        proc_seq.append(curr_dropoff)
+    return proc_seq
+
 if __name__ == '__main__':
     seqs = defaultdict(list)
+    nbrhood_polys = GetNeighborhoods()
 
-    with open('data/trip_data_1.csv', 'rb') as csvfile:
-        trips = csv.DictReader(csvfile)
-        for trip in trips:
-            lat = float(trip['pickup_latitude'])
-            long = float(trip['pickup_longitude'])
-            pickup_nbrhood = FindNeighborhood(lat, long)
-            pickup_time = datetime(trip['pickup_datetime'])
-            
-            lat = float(trip['dropoff_latitude'])
-            long = float(trip['dropoff_longitude'])
-            dropoff_nbrhood = FindNeighborhood(lat, long)
-            dropoff_time = datetime(trip['dropoff_datetime'])
+    for i in range(1, 2):
+        data_file = 'data/trip_data_%d.csv' % i
+        sys.stderr.write('Reading from %s...\n' % data_file)
+        with open(data_file, 'rb') as csvfile:
+            trips = csv.DictReader(csvfile)
+            for i, trip in enumerate(trips):
+                lat = float(trip['pickup_latitude'])
+                long = float(trip['pickup_longitude'])
+                pickup_nbrhood = FindNeighborhood(lat, long, nbrhood_polys)
+                pickup_time = format_datetime(trip['pickup_datetime'])
+                
+                lat = float(trip['dropoff_latitude'])
+                long = float(trip['dropoff_longitude'])
+                dropoff_nbrhood = FindNeighborhood(lat, long, nbrhood_polys)
+                dropoff_time = format_datetime(trip['dropoff_datetime'])
+                
+                medallion = trip['medallion']
+                
+                seqs[medallion].append((pickup_time, pickup_nbrhood, dropoff_nbrhood))
+                
+                if i % 10000 == 0:
+                    sys.stderr.write(str(i) + '\n')
 
-            medallion = trip['medallion']
-
-            seqs[medallion].append((pickup_time, pickup_nbrhood, dropoff_nbrhood))
-
-        for medallion in seqs:
-            seq = seqs[medallion]
-            seq.sort()
-            print [x[1] for x in seq]
-    
-
-
-
+        for _, seq in seqs.iteritems():
+            sys.stdout.write('\t'.join(ProcessSeq(seq)) + '\n')
