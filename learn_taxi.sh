85ce6aa6ab4c4a159b5fea1f499a315a0f54c7f5
arbenson <arbenson@gmail.com>
2016-01-05 17:02:47 -0800

    synthetic experiments

---
+++ learn_taxi.sh
@@ -0,0 +1,9 @@
+dir=processed_data/
+
+./learn_synthetic \
+    $dir/manhattan-year-train.txt \
+    $dir/manhattan-year-test.txt \
+    $dir/P \
+    $dir/manhattan-year-learned.txt \
+    > $dir/manhattan-year-data.txt
+
